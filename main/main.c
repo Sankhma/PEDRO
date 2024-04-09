5531c22ad1f480f62a0b536da9bf0968671e96da
Sankhma <thedomiprince@gmail.com>
2024-04-09 02:25:05 +0100

    Added softAP

--- main/main.c
+++ main/main.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
-void app_main(void)
-{
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
+#include "nvs_flash.h"
+
+#include "esp_log.h"
+
+#include "esp_netif.h"
+#include "esp_wifi.h"
+
+#define WIFI_SSID "don't connect"
+
+static const char *TAG = "main";
+
+void app_main(void) {
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_LOGD(TAG, "nvs_flash_init succesfull!\n");
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
+
+    ESP_ERROR_CHECK(esp_wifi_init(&config));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = WIFI_SSID,
+            .ssid_len = strlen(WIFI_SSID),
+            .password = "",
+            .authmode = WIFI_AUTH_OPEN,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    esp_wifi_start();
 }
